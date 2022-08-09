66ab9ff42ae7a6040be2862d64ae64862e3e1bc4
Ethan SAFAR <esafar@e1r9p5.clusters.42paris.fr>
2022-08-10 01:50:28 +0200

    implemented last tests for ex

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 18:41:27 by esafar            #+#    #+#             */
-/*   Updated: 2022/08/10 01:37:38 by esafar           ###   ########.fr       */
+/*   Updated: 2022/08/10 01:49:38 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,77 @@
 
 int main()
 {
-    Span sp = Span(20000);
-    // sp.addNumber(6);
-    // sp.addNumber(3);
-    // sp.addNumber(17);
-    // sp.addNumber(9);
-    // sp.addNumber(11);
-    // std::cout << sp.shortestSpan() << std::endl;
-    // std::cout << sp.longestSpan() << std::endl;
+    try
+    {
+        std::cout << CYAN "======== Test with valid amount of numbers ========" END << std::endl;
+        Span span = Span(5);
+        
+        span.addNumber(6);
+        span.addNumber(3);
+        span.addNumber(17);
+        span.addNumber(9);
+        span.addNumber(11);
+        
+        std::cout << span.shortestSpan() << std::endl;
+        std::cout << span.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-    sp.autoAddNumber(5, 20);
-    sp.printVectorArray();
+    try
+    {
+        std::cout << CYAN "======== Test with invalid amount of numbers ========" END << std::endl;
+        Span span = Span(5);
+        
+        span.addNumber(6);
+        span.addNumber(3);
+        span.addNumber(17);
+        span.addNumber(9);
+        span.addNumber(11);
 
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+        span.addNumber(42);
+        
+        std::cout << span.shortestSpan() << std::endl;
+        std::cout << span.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-    // try
-    // {
-    //     Span span(100);
+    try
+    {
+        std::cout << CYAN "======== Test with 5 numbers generated from 0 to 50 ========" END << std::endl;
+        Span span = Span(100);
 
-    //     for (int i = 10; i > 0; i--)
-    //         span.addNumber(i);
+        span.autoAddNumber(5, 50);
+        span.printVectorArray();
 
-    //     span.addNumber(42);
+        std::cout << span.shortestSpan() << std::endl;
+        std::cout << span.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    try
+    {
+        std::cout << CYAN "======== Test with 10000 numbers generated ========" END << std::endl;
+        Span span = Span(20000);
 
-    //     std::cout << "longest span: " << span.longestSpan() << std::endl;
-    //     std::cout << "shortest span: " << span.shortestSpan() << std::endl;
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
+        span.autoAddNumber(10000, 10000);
+        span.printVectorArray();
+
+        std::cout << span.shortestSpan() << std::endl;
+        std::cout << span.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     
     return (0);
 }
