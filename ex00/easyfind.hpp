8c0fb144177971cec54170c8f6cabe58930766b7
Ethan SAFAR <esafar@e1r9p5.clusters.42paris.fr>
2022-08-09 19:56:47 +0200

    did in the c++11 manner, need now to do it in a c++98 one

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 18:42:20 by esafar            #+#    #+#             */
-/*   Updated: 2022/08/09 19:44:24 by esafar           ###   ########.fr       */
+/*   Updated: 2022/08/09 19:56:22 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,6 @@
 #include <iostream>
 #include <algorithm>
 #include "color.h"
-
-// template<typename T>
-// void    print( T const &addr ) {
-//         std::cout << addr << std::endl;
-// }
-
-// template<typename T>
-// void    iter(T const *addr, int size, void (*f)(T const &)){
-
-//         for(int i = 0; i < size; i++){
-//                 (*f)(addr[i]);
-//         }
-// }
-
-// class   ClassTest {
-//         public:
-//                 ClassTest( void ) : _n(42) { return; }
-//                 ClassTest( ClassTest const & src ) { *this = src; return ; }
-//                 ~ClassTest( void ) { return ; }
-
-//                 int     getN( void )const { return (this->_n); }
-
-//                 ClassTest       &operator=( ClassTest const & src ) { this->_n = src.getN(); return(*this); }
-//         private:
-//                 int     _n;
-// };
-// std::ostream    &operator<<(std::ostream &o, ClassTest const &src) { o << src.getN(); return (o); }
 
 class NotFoundException : public std::exception {
     public:
@@ -54,8 +27,10 @@ class NotFoundException : public std::exception {
 template<typename T>
 void    easyfind( T const & x, int nb ) {
     int last = sizeof(x) / sizeof(*x);
-    
-    bool exist = std::find(x, x + last, nb);
+    std::cout << CYAN "x + last = " << *(x + last) << END << std::endl;
+
+    int exist = std::find(x, x + last, nb);
+    std::cout << CYAN "Nb found? " << *std::find(x, x + last, nb) << END << std::endl;
     if (exist) {
         std::cout << "Element found\n";
     } else {
@@ -64,5 +39,21 @@ void    easyfind( T const & x, int nb ) {
     return ;
 }
 
+// in a c++11 manner
+
+// template<typename T, class C>
+// void    easyfind( T const & t, C&& nb ) {
+//     int last = sizeof(t) / sizeof(*t);
+//     std::cout << CYAN "t + last = " << *(t + last) << END << std::endl;
+
+//     bool exist = std::find(std::begin(nb), std::end(nb), t);
+//     std::cout << CYAN "Nb found? " << *std::find(t, t + last, nb) << END << std::endl;
+//     if (exist) {
+//         std::cout << "Element found\n";
+//     } else {
+//         throw NotFoundException();
+//     }
+//     return ;
+// }
 
 #endif
