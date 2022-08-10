2e041b6b652cb3eb66e0f69551273680ce7b1b7b
Ethan SAFAR <esafar@e1r9p5.clusters.42paris.fr>
2022-08-10 03:48:18 +0200

    keep improving tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 18:41:27 by esafar            #+#    #+#             */
-/*   Updated: 2022/08/10 03:29:17 by esafar           ###   ########.fr       */
+/*   Updated: 2022/08/10 03:47:22 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,61 @@ int main()
     std::cout << MAGENTA "mstack.size ->" << mstack.size() << std::endl;
     std::cout << "list.size ->" << list.size() << END << std::endl;
 
-    // mstack.push(3);
-    // mstack.push(5);
-    // mstack.push(737);
-    // //[...]
-    // mstack.push(0);
-    
-    // std::cout << mstack.top() << std::endl;
+    std::cout << CYAN "=== Pushing 3, 5, 737 and 0 ===" END << std::endl;
 
-    // MutantStack<int>::iterator it = mstack.begin();
-    // MutantStack<int>::iterator ite = mstack.end();
-    
-    // ++it;
-    // std::cout << "++*it = " << *it << std::endl;
-    // --it;
-    // std::cout << "--*it = " <<*it << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    list.push_back(3);
+    list.push_back(5);
+    list.push_back(737);
+    list.push_back(0);
 
-    // while (it != ite)
-    // {
-    //     std::cout << "*it = " << *it << std::endl;
-    //     ++it;
-    // }
-    // std::stack<int> s(mstack);
+    std::cout << WHITE "mstack.top ->" << mstack.top() << std::endl;
+    std::cout << "list.back ->" <<list.back() << END << std::endl;
+
+    std::cout << CYAN "=== Displaying size() ===" END << std::endl;
+
+    std::cout << MAGENTA "mstack.size ->" << mstack.size() << std::endl;
+    std::cout << "list.size ->" << list.size() << END << std::endl;
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    std::list<int>::iterator jt = list.begin();
+    std::list<int>::iterator jte = list.end();
+
+    std::cout << CYAN "=== Iterator test increm/decrem ===" END << std::endl;
+
+    ++it;
+    std::cout << GREY "++*it = " << *it;
+    --it;
+    std::cout << " | --*it = " << *it << std::endl;
+
+    ++jt;
+    std::cout << "++*jt = " << *jt;
+    --jt;
+    std::cout << " | --*jt = " << *jt << END << std::endl;
+
+    std::cout << CYAN "=== Printing full stack from .begin() to .end() ===" END << std::endl;
+    std::cout << GREEN "mstack: ";
+    for ( ; it != ite; it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    std::cout << "list: ";
+    for ( ; jt != jte; jt++)
+        std::cout << *jt << " ";
+    std::cout << END << std::endl;
+
+    std::cout << CYAN "=== Displaying last elem and size() ===" END << std::endl;
+
+    std::stack<int> s(mstack);
+    
+    std::cout << WHITE "s(mstack).top ->" << s.top() << std::endl;
+    std::cout << WHITE "s(mstack).size ->" << s.size() << std::endl;
+
     
     return (0);
 }
